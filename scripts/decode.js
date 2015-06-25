b354a4350657793255163a3e87c53cb74924f368
Charles-Antoine Chartrand <CharlesA.Chartrand@Gmail.com>
2015-06-24 23:52:21 -0400

    Added scripts decode and encode with multiple encodings.

---
+++ scripts/decode.js
@@ -0,0 +1,40 @@
+// Description:
+//   Decodes text in different format
+//	 Current encryptions:
+//	  - Base64
+//		- Binary
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hackerman decode <encryption> <string> - Decodes string
+//																							in the encryption chosen.
+//
+// Authors:
+//	 Charles-Antoine Chartrand
+//   David Lebrun
+
+var base64_to_utf8 = function(msg)
+{
+  msg.send(new Buffer(msg.match[1], 'base64').toString('utf8'));
+}
+
+function binary_to_utf8(msg) {
+  str = msg.match[1];
+  if(str.match(/[10]{8}/g)){
+    var wordFromBinary = str.match(/([10]{8}|\s+)/g).map(function(fromBinary){
+      return String.fromCharCode(parseInt(fromBinary, 2) );
+    }).join('');
+    msg.send(wordFromBinary);
+  }
+}
+
+module.exports = function(robot)
+{
+  robot.respond(/decode base64 (.*)/i, base64_to_utf8);
+  robot.respond(/decode binary (.*)/i, binary_to_utf8);
+}
---
+++ scripts/encode.js
@@ -0,0 +1,42 @@
+// Description:
+//   Encodes text in different format
+//	 Current encryptions:
+//	  - Base64
+//		- Binary
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hackerman encode <encryption> <string> - Encodes string
+//																							in the encryption chosen.
+//
+// Authors:
+//	 Charles-Antoine Chartrand
+//   David Lebrun
+
+var utf8_to_base64 = function(msg)
+{
+	msg.send(new Buffer(msg.match[1]).toString('base64'));
+}
+
+var utf8_to_binary = function(msg)
+{
+	var length = msg.length,
+	output = [],
+	text = msg.match[1];
+	for (var i = 0;i < length; i++) {
+		var bin = msg[i].charCodeAt().toString(2);
+		output.push(Array(8-bin.length+1).join("0") + bin);
+	}
+	msg.send(output.join(" "));
+}
+
+module.exports = function(robot)
+{
+	robot.respond(/encode base34 (.*)/i, utf8_to_base64);
+	robot.respond(/encode binary (.*)/i, utf8_to_binary);
+}
